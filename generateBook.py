1fff745f630ecbb1bf6cb523c89dbe80889a3482
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-04-14 16:27:39 +0100

    Create generateBook.py

---
+++ generateBook.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Standard libraries.
+import os
+
+# GenerateBook: Converts a collection of text and image files into a file suitible for submitting to print-on-demand services
+# (e.g. Lulu.com). Depends on Pandoc.
+
+print("Hello, world!")
