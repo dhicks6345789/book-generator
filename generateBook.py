17442d8cc6bfc45493d26b6a2b0ae0ce717b74bd
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-05-18 18:39:34 +0100

    Update generateBook.py

--- generateBook.py
+++ generateBook.py
@@ -4,8 +4,8 @@
 import os
 import sys
 
-# GenerateBook: Converts a collection of text and image files into a file suitible for submitting to print-on-demand services
-# (e.g. Lulu.com). Depends on Pandoc.
+# GenerateBook: Converts a collection of text, image and audio files into a variety of formats, including print-ready PDFs suitible for
+# submitting to print-on-demand services (e.g. Lulu.com). Depends on Pandoc, WeasyPrint, ffmpeg.
 
 def readFile(theFilename):
 	inputHandle = open(theFilename)
@@ -13,34 +13,56 @@ def readFile(theFilename):
 	inputHandle.close()
 	return result
 
+pageBreakString = "\n```{=openxml}\n<w:p><w:r><w:br w:type=\"page\"/></w:r></w:p>\n```\n"
+
+if len(sys.argv) < 2:
+	print("Usage: generateBook [rootFolder]")
+	print("This script expects to find a sub-folder called \"content\" containing files named Page 1, Page 2, etc.")
+	sys.exit(0)
+
+rootFolder = sys.argv[1]
+contentFolder = rootFolder + os.sep + "Content"
+
 page = 1
 blankPages = 0
-pageBreakString = "\n```{=openxml}\n<w:p><w:r><w:br w:type=\"page\"/></w:r></w:p>\n```\n"
-rootFolder = "C:\\Users\\Administrator.KNIGHTSBRIDGESC\\Documents\\Magoo Books\\How Good Are The Martians At Fireball"
-contentFolder = rootFolder + os.sep + "Content"
+textFiles = []
+imageFiles = []
+audioFiles = []
 items = sorted(os.listdir(contentFolder))
-outputHandle = open(rootFolder + os.sep + "interior.md","w")
+# Sort through the items in the input folder, creating (ordered) lists as we go of media to include in different versions of this book.
 while not items == []:
-	pageItem = "Page " + str(page)
+	pageItem = "Processing page " + str(page)
+	pageFound = False
 	if pageItem + ".txt" in items:
 		items.remove(pageItem + ".txt")
-		print(pageItem + ".txt")
-		outputHandle.write(readFile(contentFolder + os.sep + pageItem + ".txt") + pageBreakString)
-		blankPages = 0
-		page = page + 1
-	elif pageItem + ".png" in items:
+		textFiles.append(pageItem + ".txt")
+		pageFound = True
+	if pageItem + ".png" in items:
 		items.remove(pageItem + ".png")
-		print(pageItem + ".png")
-		outputHandle.write("![](" + rootFolder + os.sep + "Content" + os.sep + pageItem + ".png \"\")"+ pageBreakString)
+		imageFiles.append(pageItem + ".png")
+		pageFound = True
+	if pageItem + ".wav" in items:
+		items.remove(pageItem + ".wav")
+		audioFiles.append(pageItem + ".wav")
+		pageFound = True
+		
+	if pageFound:
 		blankPages = 0
 		page = page + 1
 	else:
-		print("Blank " + pageItem)
 		blankPages = blankPages + 1
 		page = page + 1
+		
 	if blankPages > 5:
 		print("Too many blank pages. Still to process:")
 		print(items)
 		sys.exit(0)
-outputHandle.close()
-os.system("pandoc -raw_attribute -s --reference-doc=reference.docx -f markdown \"" + rootFolder + os.sep + "interior.md\" -t docx -o \"" + rootFolder + os.sep + "interior.docx\"")
+
+
+
+#outputHandle = open(rootFolder + os.sep + "interior.md","w")
+#outputHandle.close()
+#os.system("pandoc -raw_attribute -s --reference-doc=reference.docx -f markdown \"" + rootFolder + os.sep + "interior.md\" -t docx -o \"" + rootFolder + os.sep + "interior.docx\"")
+
+#outputHandle.write(readFile(contentFolder + os.sep + pageItem + ".txt") + pageBreakString)
+#outputHandle.write("![](" + rootFolder + os.sep + "Content" + os.sep + pageItem + ".png \"\")"+ pageBreakString)
